38a77aeeaaeb12a6cbb058bd457911d77af6f980
Belle Noble <bellenoble@gmail.com>
2016-11-02 12:18:39 +1100

    Initial Commit

--- Resume/ViewController.swift
+++ Resume/ViewController.swift
@@ -14,12 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func emailisTapped(_ sender: AnyObject) {
+        let email = "bellenoble@gmail.com"
+        let url = NSURL(string: "mailto:\(email)")
+        UIApplication.shared.openURL(url as! URL)
+    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func callTapped(_ sender: AnyObject) {
+        if let url = NSURL(string: "tel://0409323332") {
+            UIApplication.shared.openURL(url as URL)
+        }
+        
+    }
 }
+
+
+
 
